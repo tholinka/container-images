ae1a46d157fce79b1923f9b0d63d48363c3cafc7
Tyler Holinka <1685504+tholinka@users.noreply.github.com>
2025-12-14 22:42:34 -0600

    chore: bjw-s-labs -> tholinka

--- apps/gluetun-qb-port-sync/container_test.go
+++ apps/gluetun-qb-port-sync/container_test.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"testing"
 
-	"github.com/bjw-s-labs/container-images/testhelpers"
+	"github.com/tholinka/container-images/testhelpers"
 )
 
 func Test(t *testing.T) {
 	ctx := context.Background()
-	image := testhelpers.GetTestImage("ghcr.io/bjw-s-labs/gluetun-qb-port-sync:rolling")
+	image := testhelpers.GetTestImage("ghcr.io/tholinka/gluetun-qb-port-sync:rolling")
 
 	t.Run("Check /app/script.sh exists", func(t *testing.T) {
 		testhelpers.TestFileExists(t, ctx, image, "/app/script.sh", nil)
